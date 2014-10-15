bbc7c8284ccbc8ebd24c1b1f2cb827b24cf32eff
percival <percival@172.16.18.41>
2014-10-15 13:58:35 -0300

    INITIAL VERSION

---
+++ BookWorm/apps/BookWorm/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":846016401,"date":1413391931219,"machine":"MACCIT011227.local"};
+/* Date: Wed Oct 15 13:52:11 BRT 2014 */
---
+++ BookWorm/apps/BookWorm/iphone/native/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":904820953,"date":1413383888766,"machine":"MACCIT011227.local"};
+/* Date: Wed Oct 15 11:38:08 BRT 2014 */
