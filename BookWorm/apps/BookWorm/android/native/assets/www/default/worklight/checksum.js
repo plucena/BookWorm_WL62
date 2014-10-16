2bbbd59c2496cc48cde3b0edbd51b1c2074eb09d
percival <percival@172.16.18.41>
2014-10-16 14:52:47 -0300

    NEW ICON - ANDROID

--- BookWorm/apps/BookWorm/android/native/assets/www/default/worklight/checksum.js
+++ BookWorm/apps/BookWorm/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":846016401,"date":1413391931219,"machine":"MACCIT011227.local"};
-/* Date: Wed Oct 15 13:52:11 BRT 2014 */
+var WL_CHECKSUM = {"checksum":4279026243,"date":1413481665093,"machine":"MACCIT011227.local"};
+/* Date: Thu Oct 16 14:47:45 BRT 2014 */
