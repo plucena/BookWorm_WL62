622fd757d53529e78e570972eeb97de95042d135
percival <percival@172.16.18.41>
2014-10-16 15:31:35 -0300

    WELCOME PAGE FIXES

--- BookWorm/apps/BookWorm/android/native/assets/www/default/worklight/checksum.js
+++ BookWorm/apps/BookWorm/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":4279026243,"date":1413481665093,"machine":"MACCIT011227.local"};
-/* Date: Thu Oct 16 14:47:45 BRT 2014 */
+var WL_CHECKSUM = {"checksum":1388782436,"date":1413484198700,"machine":"MACCIT011227.local"};
+/* Date: Thu Oct 16 15:29:58 BRT 2014 */
